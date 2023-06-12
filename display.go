41a8d152722a8fc7e50fc8e010034bf7853901bd
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-12 21:55:38 +0700

    handle error

--- display.go
+++ display.go
@@ -1,15 +1,23 @@
 package display
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func StartDisplay(stop chan bool) {
 	go func() {
 		cmd := exec.Command("./display/IndirectDisplay/x64/Release/IddSampleApp.exe")
-		cmd.Start()
+		err := cmd.Start()
+		if err != nil {
+			fmt.Printf("fail to start virtual display %s\n", err.Error())
+			return
+		}
 		<-stop
-		cmd.Process.Kill()
-		cmd.Wait()
+		err = cmd.Process.Kill()
+		if err != nil {
+			fmt.Printf("fail to kill virtual display %s\n", err.Error())
+			return
+		}
 	}()
 }
