1608e190c0c7c6e5c9db8a69843776fbcdd9cdd7
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-06-12 22:59:43 +0700

    simple create sample app

--- display.go
+++ display.go
@@ -1,23 +1,9 @@
 package display
 
 import (
-	"fmt"
 	"os/exec"
 )
 
-func StartDisplay(stop chan bool) {
-	go func() {
-		cmd := exec.Command("./display/IndirectDisplay/x64/Release/IddSampleApp.exe")
-		err := cmd.Start()
-		if err != nil {
-			fmt.Printf("fail to start virtual display %s\n", err.Error())
-			return
-		}
-		<-stop
-		err = cmd.Process.Kill()
-		if err != nil {
-			fmt.Printf("fail to kill virtual display %s\n", err.Error())
-			return
-		}
-	}()
+func StartDisplay() {
+	go exec.Command("./display/IndirectDisplay/x64/Release/IddSampleApp.exe").Output()
 }
