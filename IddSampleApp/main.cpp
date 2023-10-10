83b527ce14faa75bade5d44b3bfb084092ce120e
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-10-10 17:13:35 +0000

    set resolution dynamically

--- IddSampleApp/main.cpp
+++ IddSampleApp/main.cpp
@@ -32,6 +32,9 @@ int __cdecl main(int argc, wchar_t *argv[])
     SW_DEVICE_CREATE_INFO createInfo = { 0 };
     PCWSTR description = L"Idd Sample Driver";
 
+    int result;
+	int deviceIndex=0;
+
     // These match the Pnp id's in the inf file so OS will load the driver when the device is created    
     PCWSTR instanceId = L"IddSampleDriver";
     PCWSTR hardwareIds = L"IddSampleDriver\0\0";
@@ -65,20 +68,66 @@ int __cdecl main(int argc, wchar_t *argv[])
     // Wait for callback to signal that the device has been created
     printf("Waiting for device to be created....\n");
     DWORD waitResult = WaitForSingleObject(hEvent, 10*1000);
-    if (waitResult != WAIT_OBJECT_0)
-    {
+    if (waitResult != WAIT_OBJECT_0) {
         printf("Wait for device creation failed\n");
         return 1;
     }
-    printf("Device created\n\n");
-    
-    // Now wait for user to indicate the device should be stopped
-    while (true) {
-        Sleep(1000);
+    printf("Device created successfully\n");
+    Sleep(1000);
+
+    int Width = 0;
+    int Height = 0;
+    char* width = nullptr;
+    char* height = nullptr;
+    size_t sz = 0;
+    _dupenv_s(&width,  &sz, "DISPLAY_WIDTH");
+    _dupenv_s(&height, &sz, "DISPLAY_HEIGHT");
+    if(width == nullptr || height == nullptr) {
+        printf("Set to default resolution\n");
+        Width  = 1920;
+        Height = 1080;
+    } else {
+        Width  = atoi(width);
+        Height = atoi(height);
     }
+
+	do
+	{
+		PDISPLAY_DEVICE displayDevice = new DISPLAY_DEVICE();
+		displayDevice->cb = sizeof(DISPLAY_DEVICE);
+
+		result = EnumDisplayDevices(NULL, 
+			deviceIndex++, displayDevice, 0);
+		if (displayDevice->StateFlags & DISPLAY_DEVICE_ACTIVE)
+		{
+			PDISPLAY_DEVICE monitor = new DISPLAY_DEVICE();
+			monitor->cb = sizeof(DISPLAY_DEVICE);
+
+            if(wcscmp(displayDevice->DeviceString,L"IddSampleDriver Device"))
+                continue;
+
+			EnumDisplayDevices(displayDevice->DeviceName, 
+				0, monitor, 0);
+			
+			PDEVMODE dm = new DEVMODE();
+			if ( EnumDisplaySettings(displayDevice->DeviceName,
+				ENUM_CURRENT_SETTINGS, dm) )
+			{
+
+                dm->dmPelsWidth  = Width;
+                dm->dmPelsHeight = Height;
+                printf("changing resolution of display %ls to %dx%d \n",displayDevice->DeviceString,Width,Height);
+				ChangeDisplaySettingsEx(displayDevice->DeviceName, dm,  \
+                                     NULL, (CDS_GLOBAL | CDS_UPDATEREGISTRY | CDS_RESET), NULL);
+			}
+		}
+	} while (result);
+
+    // Now wait for user to indicate the device should be stopped
+    while (true) 
+        Sleep(1000);
     
     // Stop the device, this will cause the sample to be unloaded
     SwDeviceClose(hSwDevice);
-
     return 0;
 }
